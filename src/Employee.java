5ded1231b7211f27401e7c997092579cee75ba68
KRISHNA\CHANDAN <chandan115325@gmail.com>
2017-04-10 11:38:32 +0530

    no message

--- src/Employee.java
+++ src/Employee.java
@@ -48,7 +48,7 @@ class PermanentEmp extends Employee{
 		this.avail_leaves = this.total_leave_entitlement-this.balance_leave;
 		if(this.balance_leave < this.total_leave_entitlement)
 		{	
-			System.out.println("leaves are available :"+this.avail_leaves);
+			System.out.println("leaves are available.");
 		}
 		else{
 			System.out.println("leaves are not available.");
@@ -121,7 +121,7 @@ class TemporaryEmp extends Employee{
 		this.avail_leaves = this.total_leave_entitlement - this.balance_leave;
 		if(this.balance_leave < this.total_leave_entitlement)
 		{	
-			System.out.println("leaves are available :" +this.avail_leaves);
+			System.out.println("leaves are available ." );
 		}
 		else{
 			System.out.println("leaves are not available.");
